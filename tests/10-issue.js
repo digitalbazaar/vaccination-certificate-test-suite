c1111f22d96926780e0d5e565479cde71901c450
Andrew L Jones <ajones@digitalbazaar.com>
2021-03-31 16:49:31 -0400

    Start tests.

---
+++ tests/10-issue.js
@@ -0,0 +1,10 @@
+/*!
+ * Copyright (c) 2021 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+describe('issue', function() {
+  it('a credential', function() {
+    console.log('issue');
+  });
+});
